0a3864df1f3cb904b19c3e1aca9182380fb346e9
LLirik <kirill.m@doyousync.com>
2016-12-17 14:33:10 +0300

    Different browsers start

--- README.md
+++ README.md
@@ -1,6 +1,9 @@
 # hw-repo_sel-2
 Home work repository for selenium-2 group in software-testing training
 
+v.0.2.1
+ Start IE/Chrome/FF/FF ESR/FF Nightly
+
 v.0.1.1
  Entering admin area of litechart on localhost:8080
 
--- Simple Test.py
+++ Simple Test.py
@@ -1,6 +1,8 @@
 from selenium import webdriver
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.firefox.firefox_binary import FirefoxBinary
+
 import pytest
 '''
 
@@ -10,25 +12,46 @@ import pytest
 
 
 @pytest.fixture
-def driver(request):
+def f_driver(request):
     wd = webdriver.Firefox()
     print(wd.capabilities)
     request.addfinalizer(wd.quit)
     return wd
 
-def test1(driver):
+@pytest.fixture
+def f_n_driver(request):
+
+    binary = FirefoxBinary('C:\Program Files\Nightly\\firefox.exe')
+    wd = webdriver.Firefox(firefox_binary=binary)
+    print(wd.capabilities)
+    request.addfinalizer(wd.quit)
+    return wd
+
+@pytest.fixture
+def i_driver(request):
+    wd = webdriver.Ie()
+    print(wd.capabilities)
+    request.addfinalizer(wd.quit)
+    return wd
+
+@pytest.fixture
+def ch_driver(request):
+    wd = webdriver.Chrome()
+    print(wd.capabilities)
+    request.addfinalizer(wd.quit)
+    return wd
+
+def test_f_n(f_n_driver):
     #Please check the PORT!!!!!
-    driver.get("http://localhost:8080/litecart/admin/login.php")
+    f_n_driver.get("http://localhost:8080/litecart/admin/login.php")
 
-    WebDriverWait(driver, 10).until(EC.title_contains("My Store"))
+    WebDriverWait(f_n_driver, 10).until(EC.title_contains("My Store"))
 
-    login_box = driver.find_element_by_name("username")
+    login_box = f_n_driver.find_element_by_name("username")
     login_box.send_keys("admin")
 
-    password_box = driver.find_element_by_name("password")
+    password_box = f_n_driver.find_element_by_name("password")
     password_box.send_keys("admin")
 
-    login_btn = driver.find_element_by_name("login")
+    login_btn = f_n_driver.find_element_by_name("login")
     login_btn.click()
-
-    driver.quit()
